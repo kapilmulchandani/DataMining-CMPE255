aea45cefb0061f188f6313422fd96c0f3fe0874e
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-18 01:35:43 -0800

    Equal-width Binning on proline column of wine data

--- HW4/DataReduction.py
+++ HW4/DataReduction.py
@@ -10,9 +10,18 @@ print(wine_df['proline'].min())
 print(wine_df.shape[0])
 width = ((wine_df['proline'].max() - wine_df['proline'].min())/wine_df.shape[0])
 print(width)
+labelsnew = [0, 1, 2, 3, 4]
+wine_df['bin'] = pd.cut(wine_df['proline'], 5, labels=labelsnew)
+sumOfBins = [0, 0, 0, 0, 0]
+countOfBins = [0, 0, 0, 0, 0]
+averageOfBins = [0, 0, 0, 0, 0]
+print(wine_df['bin'][1])
+for i in range(0, wine_df.shape[0]):
+    sumOfBins[wine_df['bin'][i]] = sumOfBins[wine_df['bin'][i]] + wine_df['proline'][i]
+    countOfBins[wine_df['bin'][i]] = countOfBins[wine_df['bin'][i]] + 1;
 
-print(pd.cut(wine_df['proline'], 5))
-print(width)
-# for index, row in wine_df.iterrows():
-#     if(0 < wine_df['proline'] < width):
-#         wine_df
+for i  in range(0, len(sumOfBins)):
+    averageOfBins[i] = sumOfBins[i]/countOfBins[i]
+
+for i in range(0, wine_df.shape[0]):
+    wine_df['proline'][i] = averageOfBins[wine_df['bin'][i]]
