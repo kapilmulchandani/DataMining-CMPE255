cf108983b705885f7ffc4ca1e1ea344aefccc5bc
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-18 02:32:44 -0800

    Loop included for finding centroids

--- HW4/kMeansPrime.py
+++ HW4/kMeansPrime.py
@@ -7,39 +7,41 @@ firstTenPrimes = [[2,3], [5,7], [11,13], [17,19], [23,29], [31,37], [41,43], [47
 # 2nd : [5,7], [11,13], [17,19], [23,29], [31,37], [41,43], [47, 53], [59,61], [67, 71]
 cluster1 = [[2,3]]
 cluster2 = [[5,7], [11,13], [17,19], [23,29], [31,37], [41,43], [47, 53], [59,61], [67, 71]]
-newCentroids1 = [2/1, 3/1]
+newCentroids1 = [0, 0]
 newCentroids2 = [0, 0]
-print(firstTenPrimes[0])
-for i in range(1, len(firstTenPrimes)):
-    newCentroids2[0] = newCentroids2[0] + firstTenPrimes[i][0]
-    newCentroids2[1] = newCentroids2[1] + firstTenPrimes[i][1]
+count = 3
+while count != 0:
+    for i in range(0, len(cluster1)):
+        newCentroids1[0] = newCentroids1[0] + cluster1[i][0]
+        newCentroids1[1] = newCentroids1[1] + cluster1[i][1]
 
-newCentroids2[0] = newCentroids2[0]/len(firstTenPrimes) - 1
-newCentroids2[1] = newCentroids2[1]/len(firstTenPrimes) - 1
+    newCentroids1[0] = newCentroids1[0]/len(cluster1)
+    newCentroids1[1] = newCentroids1[1]/len(cluster1)
 
-print(newCentroids2)
-print(newCentroids1)
+    for i in range(0, len(cluster2)):
+        newCentroids2[0] = newCentroids2[0] + cluster2[i][0]
+        newCentroids2[1] = newCentroids2[1] + cluster2[i][1]
 
-#Now compute Eucleadian distance of points to both the centroids
-distancesFrom1 = []
-for i in range(0, len(firstTenPrimes)):
-    distancesFrom1.append(distance.euclidean(firstTenPrimes[i], newCentroids1))
+    newCentroids2[0] = newCentroids2[0]/len(cluster2)
+    newCentroids2[1] = newCentroids2[1]/len(cluster2)
 
-distancesFrom2 = []
-for i in range(0, len(firstTenPrimes)):
-    distancesFrom2.append(distance.euclidean(firstTenPrimes[i], newCentroids2))
+    #Now compute Eucleadian distance of points to both the centroids
+    distancesFrom1 = []
+    for i in range(0, len(firstTenPrimes)):
+        distancesFrom1.append(distance.euclidean(firstTenPrimes[i], newCentroids1))
 
-print(distancesFrom1)
-j = 0
-k = 0
-cluster1=[]
-cluster2=[]
-for i in range(0, len(firstTenPrimes)):
-    if distancesFrom1[i] < distancesFrom2[i]:
-        cluster1.append(firstTenPrimes[i])
-    else:
-        cluster2.append(firstTenPrimes[i])
+    distancesFrom2 = []
+    for i in range(0, len(firstTenPrimes)):
+        distancesFrom2.append(distance.euclidean(firstTenPrimes[i], newCentroids2))
 
-print('wow')
-# Now Change the centroids and clusters accordingly
+    # Now Change the centroids and clusters accordingly
+    cluster1=[]
+    cluster2=[]
+    for i in range(0, len(firstTenPrimes)):
+        if distancesFrom1[i] < distancesFrom2[i]:
+            cluster1.append(firstTenPrimes[i])
+        else:
+            cluster2.append(firstTenPrimes[i])
+    count = count-1
+
 
