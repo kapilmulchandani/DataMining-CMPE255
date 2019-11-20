d881431cb67c28f60abdff7e36272a268af3bd70
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-19 23:01:32 -0800

    Data reduction on entire dataframe

--- HW4/DataReduction.py
+++ HW4/DataReduction.py
@@ -1,27 +1,25 @@
 from sklearn.datasets import load_wine
 import pandas as pd
-import chart_studio.plotly as ply
-import matplotlib.pylab as plt
 
 wine_data = load_wine()
 wine_df = pd.DataFrame(wine_data.data, columns=wine_data.feature_names)
-print(wine_df['proline'].max())
-print(wine_df['proline'].min())
-print(wine_df.shape[0])
-width = ((wine_df['proline'].max() - wine_df['proline'].min())/wine_df.shape[0])
-print(width)
-labelsnew = [0, 1, 2, 3, 4]
-wine_df['bin'] = pd.cut(wine_df['proline'], 5, labels=labelsnew)
-sumOfBins = [0, 0, 0, 0, 0]
-countOfBins = [0, 0, 0, 0, 0]
-averageOfBins = [0, 0, 0, 0, 0]
-print(wine_df['bin'][1])
-for i in range(0, wine_df.shape[0]):
-    sumOfBins[wine_df['bin'][i]] = sumOfBins[wine_df['bin'][i]] + wine_df['proline'][i]
-    countOfBins[wine_df['bin'][i]] = countOfBins[wine_df['bin'][i]] + 1;
+for key, value in wine_df.iteritems():
+    width = ((value.max() - value.min())/wine_df.shape[0])
+    labelsnew = [0, 1, 2, 3, 4]
+    wine_df['bin'] = pd.cut(value, 5, labels=labelsnew)
+    sumOfBins = [0, 0, 0, 0, 0]
+    countOfBins = [0, 0, 0, 0, 0]
+    averageOfBins = [0, 0, 0, 0, 0]
+    print(wine_df['bin'][1])
+    for i in range(0, wine_df.shape[0]):
+        sumOfBins[wine_df['bin'][i]] = sumOfBins[wine_df['bin'][i]] + value[i]
+        countOfBins[wine_df['bin'][i]] = countOfBins[wine_df['bin'][i]] + 1;
 
-for i  in range(0, len(sumOfBins)):
-    averageOfBins[i] = sumOfBins[i]/countOfBins[i]
+    for i  in range(0, len(sumOfBins)):
+        averageOfBins[i] = sumOfBins[i]/countOfBins[i]
 
-for i in range(0, wine_df.shape[0]):
-    wine_df['proline'][i] = averageOfBins[wine_df['bin'][i]]
+    for i in range(0, wine_df.shape[0]):
+        value[i] = averageOfBins[wine_df['bin'][i]]
+
+
+wine_df = wine_df.drop(['bin'], axis=1)
