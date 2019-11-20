08e126cdc6662da7a9ff210ec4668f9cdef37665
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-19 23:19:13 -0800

    Generalized The cluster making

--- HW4/kMeansPrime.py
+++ HW4/kMeansPrime.py
@@ -2,14 +2,31 @@ from scipy.spatial import distance
 
 firstTenPrimes = [[2,3], [5,7], [11,13], [17,19], [23,29], [31,37], [41,43], [47, 53], [59,61], [67, 71]]
 
-#First we make 2 clusters -->
-# 1st : [2,3]
-# 2nd : [5,7], [11,13], [17,19], [23,29], [31,37], [41,43], [47, 53], [59,61], [67, 71]
-cluster1 = [[2,3]]
-cluster2 = [[5,7], [11,13], [17,19], [23,29], [31,37], [41,43], [47, 53], [59,61], [67, 71]]
+# Just change the centroid for the second part [[2,3], [67,71]]
+givenCentroid1 = [2,3]
+givenCentroid2 = [5,7]
+
 newCentroids1 = [0, 0]
 newCentroids2 = [0, 0]
-count = 3
+
+cluster1=[]
+cluster2=[]
+
+distancesFrom1 = []
+for i in range(0, len(firstTenPrimes)):
+    distancesFrom1.append(distance.euclidean(firstTenPrimes[i], givenCentroid1))
+
+distancesFrom2 = []
+for i in range(0, len(firstTenPrimes)):
+    distancesFrom2.append(distance.euclidean(firstTenPrimes[i], givenCentroid2))
+
+for i in range(0, len(firstTenPrimes)):
+    if distancesFrom1[i] < distancesFrom2[i]:
+        cluster1.append(firstTenPrimes[i])
+    else:
+        cluster2.append(firstTenPrimes[i])
+
+count = 5
 while count != 0:
     for i in range(0, len(cluster1)):
         newCentroids1[0] = newCentroids1[0] + cluster1[i][0]
