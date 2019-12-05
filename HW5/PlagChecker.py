e2908857a824ea67a1ee7a255cf44982e0e617ae
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-12-04 23:56:25 -0800

    Sine Similarity Included

--- HW5/PlagChecker.py
+++ HW5/PlagChecker.py
@@ -11,6 +11,19 @@ q = ""
 def loadPage():
     return render_template('index.html', query="")
 
+@app.route("/euclid", methods=['POST'])
+def euclideanSimilarity():
+    queryTF = []
+    databaseTF = []
+    dotProduct = 0
+    sumOfSquares = 0
+    for i in range(len(queryTF)):
+        dotProduct += queryTF[i] * databaseTF[i]
+        sumOfSquares += (queryTF[i] - databaseTF[i]) ** 2
+    euclideanDistance = math.sqrt(sumOfSquares)
+
+    # euclidSimilarity = (100-(euclideanDistance*22))/100
+    euclidSimilarity = (1 / 1 + euclideanDistance) * 100
 
 @app.route("/", methods=['POST'])
 def cosineSimilarity():
@@ -67,7 +80,8 @@ def cosineSimilarity():
         sumOfSquares += (queryTF[i] - databaseTF[i]) ** 2
     euclideanDistance = math.sqrt(sumOfSquares)
 
-    euclidSimilarity = (100-(euclideanDistance*22))/100
+    # euclidSimilarity = (100-(euclideanDistance*22))/100
+    euclidSimilarity = (1/1+euclideanDistance)*100
 
     queryVectorMagnitude = 0
     for i in range(len(queryTF)):
@@ -96,5 +110,85 @@ def cosineSimilarity():
 
     return render_template('index.html', query=inputQuery, output=output)
 
+@app.route("/sine", methods=['POST'])
+def sineSimilarity():
+    universalSetOfUniqueWords = []
+    matchPercentage = 0
+
+    ####################################################################################################
+
+    inputQuery = request.form['query']
+    lowercaseQuery = inputQuery.lower()
+
+    queryWordList = re.sub("[^\w]", " ", lowercaseQuery).split()  # Replace punctuation by space and split
+    # queryWordList = map(str, queryWordList)					#This was causing divide by zero error
+
+    for word in queryWordList:
+        if word not in universalSetOfUniqueWords:
+            universalSetOfUniqueWords.append(word)
+
+    ####################################################################################################
+
+    fd = open("database1.txt", "r")
+    database1 = fd.read().lower()
+
+    databaseWordList = re.sub("[^\w]", " ", database1).split()  # Replace punctuation by space and split
+    # databaseWordList = map(str, databaseWordList)			#And this also leads to divide by zero error
+
+    for word in databaseWordList:
+        if word not in universalSetOfUniqueWords:
+            universalSetOfUniqueWords.append(word)
+
+    ####################################################################################################
+
+    queryTF = []
+    databaseTF = []
+
+    for word in universalSetOfUniqueWords:
+        queryTfCounter = 0
+        databaseTfCounter = 0
+
+        for word2 in queryWordList:
+            if word == word2:
+                queryTfCounter += 1
+        queryTF.append(queryTfCounter)
+
+        for word2 in databaseWordList:
+            if word == word2:
+                databaseTfCounter += 1
+        databaseTF.append(databaseTfCounter)
+
+    crossProduct = 0
+    sumOfSquares = 0
+    for i in range(len(queryTF)):
+        crossProduct += queryTF[i] * databaseTF[i]
+        sumOfSquares += (queryTF[i] - databaseTF[i]) ** 2
+
+    queryVectorMagnitude = 0
+    for i in range(len(queryTF)):
+        queryVectorMagnitude += queryTF[i] ** 2
+    queryVectorMagnitude = math.sqrt(queryVectorMagnitude)
+
+    databaseVectorMagnitude = 0
+    for i in range(len(databaseTF)):
+        databaseVectorMagnitude += databaseTF[i] ** 2
+    databaseVectorMagnitude = math.sqrt(databaseVectorMagnitude)
+
+    matchPercentage = (float)(crossProduct / (queryVectorMagnitude * databaseVectorMagnitude)) * 100
+
+    '''
+    print queryWordList
+    print
+    print databaseWordList
+
+
+    print queryTF
+    print
+    print databaseTF
+    '''
+
+    output = "Input query text matches %0.02f%% with database." % matchPercentage
+
+    return render_template('index.html', query=inputQuery, output=output)
 
 app.run()
